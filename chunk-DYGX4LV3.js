752ae22b84836cbe4476ed321a9c7b21a10d4c95
tazlin <tazlin.on.github@gmail.com>
2024-06-10 18:16:13 -0400

    Auto-generated commit

---
+++ chunk-DYGX4LV3.js
@@ -0,0 +1,1 @@
+import{$ as o,jb as s,kb as r,qb as i,tb as a}from"./chunk-HTB7U7SQ.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-sponsors"]],standalone:!0,features:[a],decls:2,vars:0,template:function(n,c){n&1&&(s(0,"p"),i(1,"sponsors works!"),r())}});let t=e;return t})();export{d as SponsorsComponent};
--- chunk-WRLZL7TQ.js
+++
@@ -1,1 +0,0 @@
-import{aa as o,kb as s,lb as r,rb as i,ub as a}from"./chunk-JJ2BSIYJ.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-sponsors"]],standalone:!0,features:[a],decls:2,vars:0,template:function(n,c){n&1&&(s(0,"p"),i(1,"sponsors works!"),r())}});let t=e;return t})();export{d as SponsorsComponent};
