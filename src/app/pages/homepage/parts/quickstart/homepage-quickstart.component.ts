3c5f2e3fa8b6834f7ec52a2fee075fb836e15e4b
tazlin <tazlin.on.github@gmail.com>
2025-11-02 06:40:36 -0500

    fix: GUIs route and improve navigation links
    - Changed all references from '/guis-and-tools' to '/guis' for consistency and updated routing logic to redirect accordingly.
    - Refactored navigation and feature card links to use Angular's RouterLink where appropriate.
    - Improved tooltip styling for better mobile responsiveness and clarified stats tooltip text.
    - Fix linting

--- src/app/pages/homepage/parts/quickstart/homepage-quickstart.component.ts
+++ src/app/pages/homepage/parts/quickstart/homepage-quickstart.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslocoPipe, TranslocoModule } from '@jsverse/transloco';
 import { InlineSvgComponent } from '../../../../components/inline-svg/inline-svg.component';
 
 @Component({
   selector: 'app-homepage-quickstart',
   standalone: true,
-  imports: [TranslocoPipe, TranslocoModule, InlineSvgComponent],
+  imports: [RouterLink, TranslocoPipe, TranslocoModule, InlineSvgComponent],
   templateUrl: './homepage-quickstart.component.html',
   styleUrl: './homepage-quickstart.component.css',
 })
