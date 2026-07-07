096dfee03495cbae9df3a3fef1dd74182302763f
tazlin <tazlin.on.github@gmail.com>
2026-07-06 23:38:53 -0400

    Auto-generated commit

---
+++ chunk-OHQ5WGL3.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var o=[{path:"",redirectTo:"workers",pathMatch:"full"},{path:"workers",loadComponent:()=>import("./chunk-L7SU4FLY.js").then(e=>e.WorkersComponent)},{path:"joining",redirectTo:"workers",pathMatch:"full"},{path:"donate",loadComponent:()=>import("./chunk-3YEDDHRT.js").then(e=>e.DonateComponent)}];export{o as contributeRoutes};
